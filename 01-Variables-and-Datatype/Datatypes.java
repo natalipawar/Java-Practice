0fb0d29b774356e08110ebb656add4b18ad088cf
natali pawar <natalipawar>
2026-08-03 19:36:53 +0530

    Added Datatypes file

---
+++ 01-Variables-and-Datatype/Datatypes.java
@@ -0,0 +1,28 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        
+        byte b = 10;
+        System.out.println(b);
+
+        char ch = 'A';
+        System.out.println(ch);
+
+        boolean bool = true;
+        System.out.println(bool);
+
+        int num = 100;
+        System.out.println(num);
+
+        float f = 10.5f;
+        System.out.println(f);
+
+        double d = 20.5;
+        System.out.println(d);
+
+        short s = 105;
+        System.out.println(s);
+
+        long l = 1000L;
+        System.out.println(l);
+    }    
+}
