1495c1cb53c2c529ee23cbef6c22017963193aff
natali pawar <natalipawar>
2026-08-03 19:07:48 +0530

    Added java basics file

--- 01-Variables-and-Datatype/JavaBasics.java
+++ 01-Variables-and-Datatype/JavaBasics.java
@@ -1,7 +1,15 @@
+// boilerplate code 
+
 public class JavaBasics {
     public static void main(String[] args) {
-    
+
         System.out.print("Hello World");
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+        System.out.print("\n");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
     }
 }
-// boilerplate code 
