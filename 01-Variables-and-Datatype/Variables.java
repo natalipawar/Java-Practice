3d10536b7976bab689b0f5d42adf29365b80e3a5
natali pawar <natalipawar>
2026-08-03 19:29:24 +0530

    Added Variables file

---
+++ 01-Variables-and-Datatype/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+   public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Natali";
+        System.out.println(name);
+
+        a = b;
+        System.out.println(a);
+
+        b=50;
+        System.out.println(b);
+   } 
+}
