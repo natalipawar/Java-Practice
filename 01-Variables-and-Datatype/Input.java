e31b22cdb20fb0c16be8056f6c1f1ba4f398619d
natali pawar <natalipawar>
2026-08-04 16:18:43 +0530

    Added input file

---
+++ 01-Variables-and-Datatype/Input.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // String name = sc.next();
+        // System.out.println(name);
+
+        // String name1 = sc.nextLine();
+        // System.out.println(name1);
+
+        // int num = sc.nextInt();
+        // System.out.println(num);
+
+        // float num = sc.nextFloat();
+        // System.out.println(num);
+
+        // Boolean bool = sc.nextBoolean();
+        // System.out.println(bool);
+
+        sc.close();
+    }
+}
