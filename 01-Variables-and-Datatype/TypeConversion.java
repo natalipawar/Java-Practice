36015e96a27932a93889423a17790f779895f935
natali pawar <natalipawar>
2026-08-04 22:34:12 +0530

    Added Type Conversion file

---
+++ 01-Variables-and-Datatype/TypeConversion.java
@@ -0,0 +1,21 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+
+        // int a = 10;
+        // long b = a;
+
+        // float a = 20.03f;
+        // int b = a;
+        // int b = (int) a;
+
+        // System.out.println(b);
+
+        char a = 'A';
+        int num = a;
+        char b = 'B';
+        int num2 = b;
+        System.out.println(num);
+        System.out.println(num2);
+
+    }
+}
