e2d405b405d8d2cb6f189bd20118333e61a7963f
Natali Pawar <natalipawar@gmail.com>
2026-08-15 16:19:11 +0530

    Added while loop file

---
+++ 04-Loops/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 50) {
+            System.out.println("Hello World");
+            count++;
+        }
+        System.out.println("END!");
+    }
+}
