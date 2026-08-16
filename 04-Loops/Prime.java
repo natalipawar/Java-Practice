12528e499f8fe1cfd6633d3ae051b391a940ebeb
Natali Pawar <natalipawar@gmail.com>
2026-08-16 11:23:52 +0530

    Solved practice questions for loops

---
+++ 04-Loops/Prime.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n <= 1) {
+            System.out.println("Not a prime.");
+        }
+        else {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= n - 1; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                }
+            }
+
+            if (isPrime == true) {
+                System.out.println("number is Prime.");
+            }
+            else {
+                System.out.println("Not a prime.");
+            }
+
+        }
+        sc.close();
+    }
+}
