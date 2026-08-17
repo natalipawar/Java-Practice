7158c2f83384ece3aa3ab10115d9e01c338c5fd2
Natali Pawar <natalipawar@gmail.com>
2026-08-17 16:11:57 +0530

    Checking all commits

--- 04-Loops/Prime.java
+++ 04-Loops/Prime.java
@@ -13,7 +13,7 @@ public class Prime {
         else {
             boolean isPrime = true;
 
-            for (int i = 2; i <= n - 1; i++) {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
                 if (n % i == 0) {
                     isPrime = false;
                 }
