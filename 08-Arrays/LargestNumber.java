72ce8c0cdae998ada2e4041e70d205767d42cf54
Natali Pawar <natalipawar@gmail.com>
2026-09-15 23:52:33 +0530

    learn: lenear search, largest number

---
+++ 08-Arrays/LargestNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class LargestNumber {
+
+    public static int largestNumber(int numbers[]) {
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i > largest) {
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        int numbers[] = { 1, 2, 6, 3, 5 };
+
+        int largest = largestNumber(numbers);
+        System.out.println("Largest is "+largest);
+
+    }
+}
